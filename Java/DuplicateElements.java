fcba0a9c2ac3876b8e80aff1ea87c97cb4d39e04
sagar pandey <sagarbaba0101@gmail.com>
2026-06-08 00:58:33 +0530

    Duplicate Element

---
+++ Java/DuplicateElements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DuplicateElements {
+     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the elements of array");
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+     }
+}
