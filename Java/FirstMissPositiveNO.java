153ec4d0d3eb5ac83cc0ff3432798627c2503501
sagar pandey <sagarbaba0101@gmail.com>
2026-06-11 23:29:21 +0530

    First Missing Positive Integer

---
+++ Java/FirstMissPositiveNO.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FirstMissPositiveNO {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the elements of array");
+            arr[i] = sc.nextInt();
+        }
+        for(int i=1;i<n+1;i++){
+            boolean found = false; 
+            for(int j=0;j<n;j++){
+                if(arr[j]==i){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                System.out.println("Missing: " + i);
+                break;
+            }
+            }
+            sc.close();
+        }
+    }
+    
