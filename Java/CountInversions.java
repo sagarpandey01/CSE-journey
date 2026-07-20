25a1a678dfbb9e8cefa99ab474771106c854cf4f
sagar pandey <sagarbaba0101@gmail.com>
2026-07-20 22:51:23 +0530

    Solved Count Inversions using BruteForce approach

---
+++ Java/CountInversions.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountInversions {
+     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
