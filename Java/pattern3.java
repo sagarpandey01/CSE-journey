9459f2f787b7ec5e0a89f0aa5f4f111821de3365
sagar pandey <sagarbaba0101@gmail.com>
2026-07-31 21:14:39 +0530

    Added Pattern Question

--- Java/pattern3.java
+++ Java/pattern3.java
@@ -1,8 +1,8 @@
 import java.util.*;
-public class pattern2 {
+public class pattern3 {
     public static void main(String[] args){
         for(int i=1;i<=4;i++){
-            for(int j=0;j<i;j++){
+            for(int j=4;j>=i;j--){
                 System.out.print("*");
             }
             System.out.println();
