16fcd9043806da1abf7120f0ba2ce5f10ee939ed
sagar pandey <sagarbaba0101@gmail.com>
2026-08-02 23:09:29 +0530

    Added pattern question

---
+++ Java/pattern5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pattern5 {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            char ch = 'A';
+            for(int j=1;j<=i;j++){
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
