021962ecff260870dc864d1519af964341c6d75f
sagar pandey <sagarbaba0101@gmail.com>
2026-08-06 21:31:16 +0530

    Added pattern question

---
+++ Java/pattern7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pattern7 {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=n;i>=1;i--){
+            char ch = 'A';
+            for(int j=1;j<=i;j++){
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
